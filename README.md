470b7aa4d56ec1fcb1f92a283859930dad3b6b95
Yemitan Isaiah Olurotimi <yemitanisaiah@gmail.com>
2020-03-28 01:10:46 +0100

    project done

--- README.md
+++ README.md
@@ -1,24 +1,32 @@
-# my-project
+# Todo App
+
+Tutorial from Traversy media
+https://www.youtube.com/watch?v=Wy9q22isx3U&t=560s
 
 ## Project setup
+
 ```
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 yarn serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 yarn build
 ```
 
 ### Lints and fixes files
+
 ```
 yarn lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <Header />
+    <router-view />
+  </div>
 </template>
 
 <script>
+import Header from "./components/layout/Header";
 export default {
-  name: "App",
-  components: {},
-  data() {
-    return {};
+  name: "app",
+  components: {
+    Header
   }
 };
 </script>
-
-<style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
-
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  line-height: 1.4;
-}
-</style>
